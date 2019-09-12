888b013888a44fd9bd4dca5e66fbf1242ecf8e65
valerio <vcrini@gmail.com>
2019-09-12 18:01:38 +0200

    adding auto creation/update of services

--- helper.py
+++ helper.py
@@ -26,6 +26,7 @@ def command(command, args, exit_on_error=True):
 
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group()
+group.add_argument('--create_or_update_service', action='store_true')
 group.add_argument('--dummy_command', action='store_true')
 group.add_argument('--docker_build', action='store_true')
 group.add_argument('--docker_pull', action='store_true')
@@ -85,6 +86,33 @@ elif args.docker_build:
         logging.info("Building {}".format(repo))
         command('docker build -t {} --cache-from {} -f {} .'.format(repo,
                                                                     repo, dockerfile), args)
+elif args.create_or_update_service:
+    cluster = os.environ['AWS_ECS_CLUSTER']
+    desired_count = os.environ['DESIRED_COUNT']
+    task_definition = os.environ['task_definition']
+    logging.info("Building ecs environment variables test")
+    for x in data:
+        name = "{}_VERSION".format(convert(x['name']))
+        os.putenv(name, x['version'])
+        logging.debug("{} -> {}".format(name, x['version']))
+    path = "/tmp/describe-services.output"
+    command("aws ecs describe-services --cluster {} --services {} > {}".format(cluster,
+                                                                               service_arn, path), args)
+    data = ""
+    logging.debug("reading json"))
+    with open(path, "r") as read_describe_services:
+        data = json.load(read_describe_services)
+    if (len(data['failures']):
+        #create
+        service_name = os.environ['AWS_SERVICE_NAME']
+        subnet = os.environ['SUBNET']
+        security_group = os.environ['SECURITY_GROUP']
+        command("aws ecs create-service --cluster {} --service-name {} --task-definition {} --launch-type EC2 --network-configuration awsvpcConfiguration={subnets={},securityGroups={}} --desired-count {} --load-balancers targetGroupArn=arn:aws:elasticloadbalancing:eu-west-1:092467779203:targetgroup/fdh-all-adv-auth-tg/e926bcd8e7d9ab12,containerName=allocation_advisorauth_server,containerPort=3001  targetGroupArn=arn:aws:elasticloadbalancing:eu-west-1:092467779203:targetgroup/fdh-all-adv-dashboard-tg/b8a053f1d0bda87f,containerName=allocation_advisor_dashboard,containerPort=3002 targetGroupArn=arn:aws:elasticloadbalancing:eu-west-1:092467779203:targetgroup/fdh-all-adv-python-tg/0382f3a4d7bdae02,containerName=allocation_advisorpython_server,containerPort=8000 ".format(cluster, service_name, task_definition, subnet, security_group, desired_count), args)
+    else:
+        #updatetask_definition
+        service_arn = os.environ['AWS_SERVICE_ARN']
+        # aws ecs update-service --cluster fdh-fastlane --service arn:aws:ecs:eu-west-1:092467779203:service/fdh-allocation-advisor --task-definition fdh-allocation-advisor:27  --force-new-deployment  --desired-count {} 
+        command("aws ecs update-service --cluster {} --service {} --task-definition {}  --force-new-deployment  --desired-count {} ".format(cluster, service_arn, task_definition, desired_count), args)
 elif args.ecs_compose_test:
     cluster = os.environ['AWS_ECS_CLUSTER']
     service_name = os.environ['AWS_SERVICE_NAME']
