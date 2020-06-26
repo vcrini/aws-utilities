64186900aae074656224943c68d9d873c47239b9
valerio <vcrini@gmail.com>
2020-06-26 19:55:27 +0200

    adding skipping capability

--- ecr_image_check.sh
+++ ecr_image_check.sh
@@ -3,19 +3,20 @@
 #    ./ecr_image_check.sh image_repo application_name version
 
 if [[ $# -lt 3 ]]; then
-    echo "Usage: $( basename $0 ) <image-repo> <application-name> <image-tag>"
+    echo "Usage: $( basename $0 ) <image-repo> <application-name> <image-tag> <optional:skip_ecr_check>"
     exit 1
 fi
 
 image_repo=$1
 application_name=$2
 app_image_version=$3
+skip_ecr_check=$4
 
 if echo $app_image_version | grep -iq snapshot; then
   app_repo=$image_repo$application_name-snapshot
 else
   app_repo=$image_repo$application_name
-  if utilities/find-ecr-image.sh $application_name $app_image_version >/dev/null; then
+  if [ -z "$skip_ecr_check" ] && utilities/find-ecr-image.sh $application_name $app_image_version >/dev/null; then
     echo "KO -> ECR image present but it's marked as immutable ... exiting"
     exit 1
   fi
