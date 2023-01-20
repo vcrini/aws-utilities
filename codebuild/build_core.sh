0d6c7e9637913e4e353d308d37384e2357f689a5
valerio <vcrini@gmail.com>
2023-01-20 14:17:19 +0100

    fixing warning for core
    fix wrong filename
    
    printing directory content
    
    adding file content
    
    fixing wrong regexp

--- codebuild/build_core.sh
+++ codebuild/build_core.sh
@@ -2,20 +2,19 @@
 #  pre_build:
 #creating dynamically an array from string
 printenv
-IFS=',' read -r -a ecr_repositories <<< "$ecr"
-aws ecr get-login-password  --region "$AWS_DEFAULT_REGION" | docker login --username AWS --password-stdin  "$account_id.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com"
-echo "$dockerhub_password" | docker login --username "$dockerhub_user" --password-stdin
-#app_image_version=v`grep -Po '(?<=^version := ")[^"]+' build.sbt`
-#proxy_image_version=v`grep -Po '(?<=^proxy_version := ")[^"]+' proxy_version.txt`
-version=(v`grep -Po '(?<=^version := ")[^"]+' build.sbt`  v`grep -Po '(?<=^proxy_version := ")[^"]+' proxy_version.txt||true`)
+IFS=',' read -r -a ecr_repositories <<< "$ECR"
+aws ecr get-login-password  --region "$AWS_DEFAULT_REGION" | docker login --username AWS --password-stdin  "$ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com"
+echo "$DOCKERHUB_PASSWORD" | docker login --username "$DOCKERHUB_USER" --password-stdin
+version=("$(grep -Po '(?<=^version := ")[^"]+' build.sbt)" "$(grep -Po '(?<=^proxy_version := ")[^"]+' proxy_version.txt||true)")
+
 ecr_urls=()
 for ((i=0; i<${#ecr_repositories[@]}; i++))
 do
   echo "ecr: ${ecr_repositories[$i]}"
   echo "version: ${version[$i]}"
-  repo=`utilities/ecr_image_check.sh $image_repo ${ecr_repositories[$i]} ${version[$i]}`
+  repo=$(utilities/ecr_image_check.sh "$IMAGE_REPO" "${ecr_repositories[$i]}" "${version[$i]}")
   echo "repo->$repo"
-  image_version=`utilities/remove_snapshot.sh ${version[$i]}`
+  image_version=$(utilities/remove_snapshot.sh "${version[$i]}")
   echo "image_version->$image_version"
   repo=$repo:${version[$i]}
   echo "repo->$repo"
@@ -30,11 +29,11 @@ aws_cred=$aws_path/credentials
 sbt_path=/root/.sbt
 sbt_cred=$sbt_path/.s3credentials
 mkdir -p $aws_path $sbt_path
-printf "[default]\nregion=$s3_aws_default_region\noutput=json" > $aws_config
+printf "[default]\nregion=%s\noutput=json" "$S3_AWS_DEFAULT_REGION" > $aws_config
 cat $aws_config
-printf "[default]\naws_access_key_id=$s3_aws_access_key_id\naws_secret_access_key=$s3_sakey" > $aws_cred
+printf "[default]\naws_access_key_id=%s\naws_secret_access_key=%s" "$S3_AWS_ACCESS_KEY_ID" "$S3_SAKEY" > $aws_cred
 cat $aws_cred
-printf "roleArn=$s3_aws_role_arn" >  $sbt_cred
+printf "roleArn=%s" "$S3_AWS_ROLE_ARN">  $sbt_cred
 cat $sbt_cred 
 BUILDS=("docker run -v $( pwd ):$( pwd )  -v $aws_path:$aws_path -v /root/.m2:/root/.m2 -v /root/.sbt:/root/.sbt -v /root/.ivy2:/root/.ivy2 -w $( pwd ) -e SBT_OPTS hseeberger/scala-sbt:8u212_1.2.8_2.12.8  sbt -no-colors -Denv=$environment $more_options clean docker:stage && cd target/docker/stage/ && docker build -t ${ecr_urls[0]} --cache-from  ${ecr_urls[0]} ." "docker build -t  ${ecr_urls[1]} --cache-from ${ecr_urls[1]} -f Dockerfile.httpd .")
 #cycling again on ecr repositories so if a single repo is given revproxy section is skipped
@@ -44,7 +43,6 @@ do
   eval "${BUILDS[$i]}" 
 done
 
-#\\  post_build:
 echo "[ECHO] Running post_build STEP at $(date)"
 echo "${ecr_urls[0]}"
 for ((i=0; i<${#ecr_urls[@]}; i++))
--- codebuild/deploy_core.sh
+++ codebuild/deploy_core.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 #  build:
 #creating dynamically an array from string
-IFS=';' read -r -a tg <<< "$target_group_ecs_cli_string"
-target_group="${tg[@]/#/--target-groups }"
+IFS=';' read -r -a tg <<< "$TARGET_GROUP_ECS_CLI_STRING"
+target_group="${tg[*]/#/--target-groups }"
 echo "target_group: $target_group"
-IFS=',' read -r -a ecr_repositories <<< "$ecr"
-version=(v`grep -Po '(?<=^version := ")[^"]+' build.sbt`  v`grep -Po '(?<=^proxy_version := ")[^"]+' proxy_version.txt||true`)
-cd target/docker/stage
-tag=`cat tag`
+IFS=',' read -r -a ecr_repositories <<< "$ECR"
+version=("$(grep -Po '(?<=^version := ")[^"]+' build.sbt)"  "$(grep -Po '(?<=^proxy_version := ")[^"]+' proxy_version.txt||true)")
+cd target/docker/stage || exit
+tag=$(cat tag)
 ecr_urls=()
 for ((i=0; i<${#ecr_repositories[@]}; i++))
 do
   echo "ecr: ${ecr_repositories[$i]}"
   echo "version: ${version[$i]}"
-  repo=`../../../utilities/ecr_image_check.sh $image_repo ${ecr_repositories[$i]} ${version[$i]}`
+  repo=$(../../../utilities/ecr_image_check.sh "$IMAGE_REPO" "${ecr_repositories[$i]}" "${version[$i]}")
   echo "repo->$repo"
-  image_version=`../../../utilities/remove_snapshot.sh ${version[$i]}` 
+  image_version=$(../../../utilities/remove_snapshot.sh "${version[$i]}") 
   echo "image_version->$image_version"
   repo=$repo:${version[$i]}
   echo "repo->$repo"
-  ecr_urls+=($repo)
+  ecr_urls+=("$repo")
 done
 #extracting old name format for compatibility with the old and avoid need to change all docker-compose using 
 # nomenclature as ${app_repo}:${app_image_version} and ${proxy_repo}:${proxy_image_version}
@@ -34,37 +34,37 @@ IFS=':' read -r -a repo_and_version <<< "$repo"
 export proxy_repo=${repo_and_version[0]}
 export proxy_image_version=${repo_and_version[1]}
 if [ "$AWS_DESIRED_COUNT" -gt "0" ]; then
-   CMD="../../../utilities/ecs-cli compose --cluster $AWS_ECS_CLUSTER --project-name $AWS_SERVICE_NAME$version_count --file docker-compose.yml --file docker-compose.aws.yml --ecs-params ecs-params.yml service up --deployment-max-percent $DEPLOYMENT_MAX_PERCENT --deployment-min-healthy-percent $DEPLOYMENT_MIN_HEALTHY_PERCENT  $target_group --force-deployment --tags $tag"
-   echo $CMD
+   CMD="../../../utilities/ecs-cli compose --cluster $AWS_ECS_CLUSTER --project-name $AWS_SERVICE_NAME --file docker-compose.yml --file docker-compose.aws.yml --ecs-params ecs-params.yml service up --deployment-max-percent $DEPLOYMENT_MAX_PERCENT --deployment-min-healthy-percent $DEPLOYMENT_MIN_HEALTHY_PERCENT  $target_group --force-deployment --tags $tag"
+   echo "$CMD"
    service_up=$(bash -c "$CMD")
    echo "service_up result is $service_up"
    else
-   CMD="../../../utilities/ecs-cli compose --cluster $AWS_ECS_CLUSTER --project-name $AWS_SERVICE_NAME$version_count --file docker-compose.yml --file docker-compose.aws.yml --ecs-params ecs-params.yml service create --deployment-max-percent $DEPLOYMENT_MAX_PERCENT --deployment-min-healthy-percent $DEPLOYMENT_MIN_HEALTHY_PERCENT  $target_group --tags $tag || true"
+   CMD="../../../utilities/ecs-cli compose --cluster $AWS_ECS_CLUSTER --project-name $AWS_SERVICE_NAME --file docker-compose.yml --file docker-compose.aws.yml --ecs-params ecs-params.yml service create --deployment-max-percent $DEPLOYMENT_MAX_PERCENT --deployment-min-healthy-percent $DEPLOYMENT_MIN_HEALTHY_PERCENT  $target_group --tags $tag || true"
    
    echo "launching service creation"
-   echo $CMD
+   echo "$CMD"
    raw_output=$(bash -c "$CMD")
-   output=$(echo $raw_output | grep -o idempotent | head -n1)
+   output=$(echo "$raw_output" | grep -o idempotent | head -n1)
    if [ "$output" = "idempotent" ]; then
       CMD="../../../utilities/ecs-cli compose --cluster $AWS_ECS_CLUSTER --project-name $AWS_SERVICE_NAME$version_count --file docker-compose.yml --file docker-compose.aws.yml --ecs-params ecs-params.yml create --tags $tag | perl -ne 'print \$1 if /TaskDefinition=.([^\"]+)\"/'"
-      echo $CMD
+      echo "$CMD"
       echo "creating new task definition"
       task_definition=$(bash -c "$CMD")
       echo "task_definition is $task_definition"
       CMD="aws ecs update-service --no-cli-pager --cluster $AWS_ECS_CLUSTER --service $AWS_SERVICE_NAME$version_count --task-definition $task_definition"
       echo "applying task definition"
-      echo $CMD
+      echo "$CMD"
       eval "$CMD"
    fi
 fi
 CMD="aws ecs describe-services  --cluster $AWS_ECS_CLUSTER  --services $AWS_SERVICE_NAME | jq '.services[0].desiredCount'"
-echo $CMD
+echo "$CMD"
 desiredCount=$(bash -c "$CMD")
 echo "desiredCount= $desiredCount"
 echo "AWS_DESIRED_COUNT= $AWS_DESIRED_COUNT"
 if [ "$AWS_DESIRED_COUNT" -ne "$desiredCount" ]; then
    CMD="../../../utilities/ecs-cli compose --cluster $AWS_ECS_CLUSTER --project-name $AWS_SERVICE_NAME$version_count service scale --deployment-max-percent $DEPLOYMENT_MAX_PERCENT --deployment-min-healthy-percent $DEPLOYMENT_MIN_HEALTHY_PERCENT $AWS_DESIRED_COUNT"
-   echo $CMD
+   echo "$CMD"
    service_scale=$(bash -c "$CMD")
    echo "service scale output is $service_scale"
 fi
