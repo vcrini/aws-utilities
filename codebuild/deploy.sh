7a3d8c637b0d9315404d7a5e6bf14dff569ec164
valerio <vcrini@gmail.com>
2023-04-11 15:10:41 +0200

    trying to fix

--- codebuild/deploy.sh
+++ codebuild/deploy.sh
@@ -9,7 +9,7 @@ echo "target_group: $target_group"
 IFS=',' read -r -a ecr_repositories <<< "$ECR"
 v=$(grep -Po '(?<=^export IMAGE_TAG=).+$' build.sh)
 #reading proxy version or take value from base image
-version=("$v" "$(grep -Po '(?<=^proxy_version := ")[^"]+' proxy_version.txt) || echo $v")
+version=("$v" "$(grep -Po '(?<=^proxy_version := ")[^"]+' proxy_version.txt || echo $v)")
 tag=$(cat tag)
 ecr_urls=()
 for ((i=0; i<${#ecr_repositories[@]}; i++))
