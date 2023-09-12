31d19cc2ef30c335c2af9426e910ece7757b203f
valerio <vcrini@gmail.com>
2023-09-12 16:00:32 +0200

    fix name

--- codebuild/dpl_build_lambda.sh
+++ codebuild/dpl_build_lambda.sh
@@ -2,7 +2,7 @@
 #  pre_build:
 #creating dynamically an array from string
 printenv
-echo "start cript"
+echo "start script"
 aws --version
 sh fake_script.sh
 aws lambda create-function --function-name $LAMBDA_NAME --zip-file fileb://lambda.zip --handler $LAMBDA_HANDLER --runtime $LAMBDA_RUNTIME --role $LAMBDA_ROLE --layers arn:aws:lambda:eu-west-1:580247275435:layer:LambdaInsightsExtension:38 arn:aws:lambda:eu-west-1:796341525871:layer:bitdpl-test-ordsimalg:2 
