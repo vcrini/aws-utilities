012842fd87a75ab83796772f57a653c5eaeee55a
valerio <vcrini@gmail.com>
2023-09-15 15:30:25 +0200

    using creation or update

--- codebuild/dpl_build_lambda.sh
+++ codebuild/dpl_build_lambda.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 #  pre_build:
 #creating dynamically an array from string
-printenv
 echo "start script"
 aws --version
 sh build_lambda.sh
-#aws lambda create-function --function-name "$LAMBDA_NAME" --zip-file fileb://lambda.zip --handler "$LAMBDA_HANDLER" --runtime "$LAMBDA_RUNTIME" --role "$LAMBDA_ROLE" --layers arn:aws:lambda:eu-west-1:580247275435:layer:LambdaInsightsExtension:38 arn:aws:lambda:eu-west-1:796341525871:layer:bitdpl-test-ordsimalg:2 
-#aws lambda get-layer-version  --layer-name bitdpl-test-ordsimalg --version-number 2
+layer2_name=$(echo "$LAMBDA_LAYER_2" | perl -ne 'print $1 if /:([^:]+)$/')
+layer2_archive=fileb://layer.zip
 get_function=$(aws lambda get-function  --function-name "$LAMBDA_NAME")
 get_layer_version=$(echo "$get_function" | jq '.Configuration|.Layers|.[1]'| jq .Arn | perl -ne 'print "$1\n" if /:(\d+)\"/')
 echo "version found: $get_layer_version"
@@ -17,13 +16,15 @@ then
   #if it fails does not return error code !=0 but fails publish
   sh build_layer.sh
   #echo "error code $?"
-  aws lambda publish-layer-version  --layer-name bitdpl-test-ordsimalg --zip-file fileb://layer.zip  
+  aws lambda publish-layer-version  --layer-name "$layer2_name" --zip-file $layer2_archive
 fi
-aws lambda update-function-configuration --function-name "$LAMBDA_NAME"  --handler "$LAMBDA_HANDLER" --runtime "$LAMBDA_RUNTIME" --role "$LAMBDA_ROLE" --layers "$LAMBDA_LAYER_1:$LAMBDA_LAYER_1_VERSION" "$LAMBDA_LAYER_2:$put_layer_version" --timeout "$LAMBDA_TIMEOUT" --memory-size "$LAMBDA_MEMORY_SIZE"
-aws lambda put-function-concurrency --function-name "$LAMBDA_NAME" --reserved-concurrent-executions "$LAMBDA_CONCURRENCY"
-aws lambda update-function-code --function-name "$LAMBDA_NAME" --zip-file fileb://lambda.zip --publish
-# ok
-aws lambda create-event-source-mapping --function-name "$LAMBDA_NAME" --event-source-arn  $QUEUE
-aws lambda list-event-source-mappings  --function-name "$LAMBDA_NAME"
+if [ "$CREATE_LAMBDA" = "true" ] 
+then
+  aws lambda create-function --function-name "$LAMBDA_NAME" --zip-file fileb://lambda.zip --handler "$LAMBDA_HANDLER" --runtime "$LAMBDA_RUNTIME" --role "$LAMBDA_ROLE" --layers "$LAMBDA_LAYER_1:$LAMBDA_LAYER_1_VERSION" "$LAMBDA_LAYER_2:$put_layer_version" --timeout "$LAMBDA_TIMEOUT" --memory-size "$LAMBDA_MEMORY_SIZE"  
+  aws lambda create-event-source-mapping --function-name "$LAMBDA_NAME" --event-source-arn  "$QUEUE"
+else
+  aws lambda update-function-configuration --function-name "$LAMBDA_NAME"  --handler "$LAMBDA_HANDLER" --runtime "$LAMBDA_RUNTIME" --role "$LAMBDA_ROLE" --layers "$LAMBDA_LAYER_1:$LAMBDA_LAYER_1_VERSION" "$LAMBDA_LAYER_2:$put_layer_version" --timeout "$LAMBDA_TIMEOUT" --memory-size "$LAMBDA_MEMORY_SIZE"
+  aws lambda update-function-code --function-name "$LAMBDA_NAME" --zip-file fileb://lambda.zip --publish
+fi
 echo "end script"
 
