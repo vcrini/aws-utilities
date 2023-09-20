0d7022732eef7c00e4498a853eb7d4b18d2aaa5a
valerio <vcrini@gmail.com>
2023-09-20 17:29:43 +0200

    removing commented code

--- codebuild/dpl_build_lambda.sh
+++ codebuild/dpl_build_lambda.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 #  pre_build:
-#creating dynamically an array from string
 echo "start script"
 parent_directory=$(dirname "$PWD")
 sh build_lambda.sh
@@ -17,7 +16,6 @@ function wait_lambda {
 	done
 }
 
-#aws lambda create-event-source-mapping --function-name "$LAMBDA_NAME" --event-source-arn  "$QUEUE"
 layer2_name=$(echo "$LAMBDA_LAYER_2" | perl -ne 'print $1 if /:([^:]+)$/')
 layer2_archive=fileb://$parent_directory/lambda_layer.zip
 lambda_archive=fileb://$parent_directory/lambda_code.zip
@@ -33,20 +31,6 @@ else
 	sh build_layer.sh
 	aws lambda publish-layer-version --layer-name "$layer2_name" --zip-file "$layer2_archive"
 fi
-#exit
-#get_function=$(aws lambda get-function --function-name "$LAMBDA_NAME")
-#exit
-#get_layer_version=$(echo "$get_function" | jq '.Configuration|.Layers|.[1]'| jq .Arn | perl -ne 'print "$1\n" if /:(\d+)\"/')
-#echo "version found: $get_layer_version"
-#put_layer_version=$(jq .version < config.json)
-#echo "version requested: $put_layer_version"
-#if [ "$get_layer_version" -ne "$put_layer_version" ]
-#then
-#  #if it fails does not return error code !=0 but fails publish
-#  sh build_layer.sh
-#  #echo "error code $?"
-#  aws lambda publish-layer-version  --layer-name "$layer2_name" --zip-file $layer2_archive
-#fi
 if [ "$CREATE_LAMBDA" = "true" ]; then
 	aws lambda create-function --function-name "$LAMBDA_NAME" --zip-file "$lambda_archive" --handler "$LAMBDA_HANDLER" --runtime "$LAMBDA_RUNTIME" --role "$LAMBDA_ROLE" --layers "$LAMBDA_LAYER_1:$LAMBDA_LAYER_1_VERSION" "$LAMBDA_LAYER_2:$requested_layer_version" --timeout "$LAMBDA_TIMEOUT" --memory-size "$LAMBDA_MEMORY_SIZE"
 	aws lambda create-event-source-mapping --function-name "$LAMBDA_NAME" --event-source-arn "$QUEUE"
