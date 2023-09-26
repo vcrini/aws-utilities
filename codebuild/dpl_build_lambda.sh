7dcc3cef7292305eaff5156d66f4d13f3280c230
valerio <vcrini@gmail.com>
2023-09-26 15:12:29 +0200

    adding double lambda

--- codebuild/dpl_build_lambda.sh
+++ codebuild/dpl_build_lambda.sh
@@ -17,10 +17,13 @@ function wait_lambda {
 }
 
 layer2_name=$(echo "$LAMBDA_LAYER_2" | perl -ne 'print $1 if /:([^:]+)$/')
-layer2_archive=fileb://$parent_directory/lambda_layer.zip
+layer2_archive=fileb://$parent_directory/lambda_layer_2.zip
+layer1_name=$(echo "$LAMBDA_LAYER_1" | perl -ne 'print $1 if /:([^:]+)$/')
+layer1_archive=fileb://$parent_directory/lambda_layer.zip
 lambda_archive=fileb://$parent_directory/lambda_code.zip
-requested_layer_version=$(jq .version <config.json)
-if aws lambda get-layer-version --layer-name "$layer2_name" --version-number "$requested_layer_version"; then
+requested_layer1_version=$(jq .lambda_layer_1_version <config.json)
+requested_layer2_version=$(jq .lambda_layer_2_version <config.json)
+if aws lambda get-layer-version --layer-name "$layer1_name" --version-number "$requested_layer1_version"; then
 	echo "version found"
 else
 	echo "error code $?"
@@ -30,13 +33,25 @@ else
 	pwd
 	ls -lht
 	ls -lht ..
+	aws lambda publish-layer-version --layer-name "$layer1_name" --zip-file "$layer1_archive"
+fi
+if aws lambda get-layer-version --layer-name "$layer2_name" --version-number "$requested_layer2_version"; then
+	echo "version found"
+else
+	echo "error code $?"
+	echo "version not found, building layer"
+	sh build_layer_2.sh
+	#check size, to remove
+	pwd
+	ls -lht
+	ls -lht ..
 	aws lambda publish-layer-version --layer-name "$layer2_name" --zip-file "$layer2_archive"
 fi
 if [ "$CREATE_LAMBDA" = "true" ]; then
-	aws lambda create-function --function-name "$LAMBDA_NAME" --zip-file "$lambda_archive" --handler "$LAMBDA_HANDLER" --runtime "$LAMBDA_RUNTIME" --role "$LAMBDA_ROLE" --layers "$LAMBDA_LAYER_1:$LAMBDA_LAYER_1_VERSION" "$LAMBDA_LAYER_2:$requested_layer_version" --timeout "$LAMBDA_TIMEOUT" --memory-size "$LAMBDA_MEMORY_SIZE"
+	aws lambda create-function --function-name "$LAMBDA_NAME" --zip-file "$lambda_archive" --handler "$LAMBDA_HANDLER" --runtime "$LAMBDA_RUNTIME" --role "$LAMBDA_ROLE" --layers "$LAMBDA_LAYER_0:$LAMBDA_LAYER_0_VERSION" "$LAMBDA_LAYER_1:$requested_layer1_version" "$LAMBDA_LAYER_2:$requested_layer2_version" --timeout "$LAMBDA_TIMEOUT" --memory-size "$LAMBDA_MEMORY_SIZE"
 	aws lambda create-event-source-mapping --function-name "$LAMBDA_NAME" --event-source-arn "$QUEUE"
 else
-	aws lambda update-function-configuration --function-name "$LAMBDA_NAME" --handler "$LAMBDA_HANDLER" --runtime "$LAMBDA_RUNTIME" --role "$LAMBDA_ROLE" --layers "$LAMBDA_LAYER_1:$LAMBDA_LAYER_1_VERSION" "$LAMBDA_LAYER_2:$requested_layer_version" --timeout "$LAMBDA_TIMEOUT" --memory-size "$LAMBDA_MEMORY_SIZE"
+	aws lambda update-function-configuration --function-name "$LAMBDA_NAME" --handler "$LAMBDA_HANDLER" --runtime "$LAMBDA_RUNTIME" --role "$LAMBDA_ROLE" --layers "$LAMBDA_LAYER_0:$LAMBDA_LAYER_0_VERSION" "$LAMBDA_LAYER_1:$requested_layer1_version" "$LAMBDA_LAYER_2:$requested_layer2_version" --timeout "$LAMBDA_TIMEOUT" --memory-size "$LAMBDA_MEMORY_SIZE"
 	wait_lambda
 	aws lambda update-function-code --function-name "$LAMBDA_NAME" --zip-file "$lambda_archive" --publish
 fi
