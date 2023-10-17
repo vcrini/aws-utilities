afe59c4b95afedea3287e03ab52c6a2f2880a82d
valerio <vcrini@gmail.com>
2023-10-17 12:36:32 +0200

    deleting manually lambda since I've no permissions

--- codebuild/dpl_build_lambda.sh
+++ codebuild/dpl_build_lambda.sh
@@ -48,6 +48,7 @@ else
   aws lambda publish-layer-version --layer-name "$layer2_name" --zip-file "$layer2_archive"
 fi
 if [ "$CREATE_LAMBDA" = "true" ]; then
+  aws lambda delete-event-source-mapping --uuid 59e58958-4482-41a7-8793-a9c33ea3c08c
   aws lambda create-function --function-name "$LAMBDA_NAME" --zip-file "$lambda_archive" --handler "$LAMBDA_HANDLER" --runtime "$LAMBDA_RUNTIME" --role "$LAMBDA_ROLE" --layers "$LAMBDA_LAYER_0:$LAMBDA_LAYER_0_VERSION" "$LAMBDA_LAYER_1:$requested_layer1_version" "$LAMBDA_LAYER_2:$requested_layer2_version" --timeout "$LAMBDA_TIMEOUT" --memory-size "$LAMBDA_MEMORY_SIZE"
   aws lambda create-event-source-mapping --function-name "$LAMBDA_NAME" --event-source-arn "$QUEUE" --batch-size "$QUEUE_BATCH_SIZE" --maximum-batching-window-in-seconds "$QUEUE_BATCH_WINDOW" --scaling-config MaximumConcurrency="$QUEUE_MAXIMUM_CONCURRENCY"
   #aws lambda create-event-source-mapping --function-name "$LAMBDA_NAME" --event-source-arn "$QUEUE2"
