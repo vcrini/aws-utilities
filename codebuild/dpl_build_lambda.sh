6d9668f8fa33195357e92fc733bb145255d2dcb0
valerio <vcrini@gmail.com>
2023-10-17 12:12:12 +0200

    adding batch size, window and maximum concurrency

--- codebuild/dpl_build_lambda.sh
+++ codebuild/dpl_build_lambda.sh
@@ -4,16 +4,16 @@ echo "start script"
 parent_directory=$(dirname "$PWD")
 sh build_lambda.sh
 function wait_lambda {
-	#for max 6 times sleeps for n seconds if function is not active
-	get_function=""
-	max_count=6
-	count=0
-	while [ "$get_function" != "ActiveSuccessful" ] && [ "$count" -lt "$max_count" ]; do
-		count=$((count = count + 1))
-		echo "$get_function"
-		get_function=$(aws lambda get-function --function-name "$LAMBDA_NAME" | jq -r '.Configuration|[.State,.LastUpdateStatus]|join("")')
-		sleep 10
-	done
+  #for max 6 times sleeps for n seconds if function is not active
+  get_function=""
+  max_count=6
+  count=0
+  while [ "$get_function" != "ActiveSuccessful" ] && [ "$count" -lt "$max_count" ]; do
+    count=$((count = count + 1))
+    echo "$get_function"
+    get_function=$(aws lambda get-function --function-name "$LAMBDA_NAME" | jq -r '.Configuration|[.State,.LastUpdateStatus]|join("")')
+    sleep 10
+  done
 }
 
 layer2_name=$(echo "$LAMBDA_LAYER_2" | perl -ne 'print $1 if /:([^:]+)$/')
@@ -24,36 +24,37 @@ lambda_archive=fileb://$parent_directory/lambda_code.zip
 requested_layer1_version=$(jq .lambda_layer_1_version <config.json)
 requested_layer2_version=$(jq .lambda_layer_2_version <config.json)
 if aws lambda get-layer-version --layer-name "$layer1_name" --version-number "$requested_layer1_version"; then
-	echo "version found"
+  echo "version found"
 else
-	echo "error code $?"
-	echo "version not found, building layer"
-	sh build_layer.sh
-	#check size, to remove
-	pwd
-	ls -lht
-	ls -lht ..
-	aws lambda publish-layer-version --layer-name "$layer1_name" --zip-file "$layer1_archive"
+  echo "error code $?"
+  echo "version not found, building layer"
+  sh build_layer.sh
+  #check size, to remove
+  pwd
+  ls -lht
+  ls -lht ..
+  aws lambda publish-layer-version --layer-name "$layer1_name" --zip-file "$layer1_archive"
 fi
 if aws lambda get-layer-version --layer-name "$layer2_name" --version-number "$requested_layer2_version"; then
-	echo "version found"
+  echo "version found"
 else
-	echo "error code $?"
-	echo "version not found, building layer"
-	sh build_layer_2.sh
-	#check size, to remove
-	pwd
-	ls -lht
-	ls -lht ..
-	aws lambda publish-layer-version --layer-name "$layer2_name" --zip-file "$layer2_archive"
+  echo "error code $?"
+  echo "version not found, building layer"
+  sh build_layer_2.sh
+  #check size, to remove
+  pwd
+  ls -lht
+  ls -lht ..
+  aws lambda publish-layer-version --layer-name "$layer2_name" --zip-file "$layer2_archive"
 fi
 if [ "$CREATE_LAMBDA" = "true" ]; then
-	aws lambda create-function --function-name "$LAMBDA_NAME" --zip-file "$lambda_archive" --handler "$LAMBDA_HANDLER" --runtime "$LAMBDA_RUNTIME" --role "$LAMBDA_ROLE" --layers "$LAMBDA_LAYER_0:$LAMBDA_LAYER_0_VERSION" "$LAMBDA_LAYER_1:$requested_layer1_version" "$LAMBDA_LAYER_2:$requested_layer2_version" --timeout "$LAMBDA_TIMEOUT" --memory-size "$LAMBDA_MEMORY_SIZE"
-	aws lambda create-event-source-mapping --function-name "$LAMBDA_NAME" --event-source-arn "$QUEUE"
+  aws lambda create-function --function-name "$LAMBDA_NAME" --zip-file "$lambda_archive" --handler "$LAMBDA_HANDLER" --runtime "$LAMBDA_RUNTIME" --role "$LAMBDA_ROLE" --layers "$LAMBDA_LAYER_0:$LAMBDA_LAYER_0_VERSION" "$LAMBDA_LAYER_1:$requested_layer1_version" "$LAMBDA_LAYER_2:$requested_layer2_version" --timeout "$LAMBDA_TIMEOUT" --memory-size "$LAMBDA_MEMORY_SIZE"
+  aws lambda create-event-source-mapping --function-name "$LAMBDA_NAME" --event-source-arn "$QUEUE" --batch-size "$QUEUE_BATCH_SIZE" --maximum-batching-window-in-seconds "$QUEUE_BATCH_WINDOW" --scaling-config MaximumConcurrency="$QUEUE_MAXIMUM_CONCURRENCY"
+  #aws lambda create-event-source-mapping --function-name "$LAMBDA_NAME" --event-source-arn "$QUEUE2"
 else
-	aws lambda update-function-configuration --function-name "$LAMBDA_NAME" --handler "$LAMBDA_HANDLER" --runtime "$LAMBDA_RUNTIME" --role "$LAMBDA_ROLE" --layers "$LAMBDA_LAYER_0:$LAMBDA_LAYER_0_VERSION" "$LAMBDA_LAYER_1:$requested_layer1_version" "$LAMBDA_LAYER_2:$requested_layer2_version" --timeout "$LAMBDA_TIMEOUT" --memory-size "$LAMBDA_MEMORY_SIZE"
-	wait_lambda
-	aws lambda update-function-code --function-name "$LAMBDA_NAME" --zip-file "$lambda_archive" --publish
+  aws lambda update-function-configuration --function-name "$LAMBDA_NAME" --handler "$LAMBDA_HANDLER" --runtime "$LAMBDA_RUNTIME" --role "$LAMBDA_ROLE" --layers "$LAMBDA_LAYER_0:$LAMBDA_LAYER_0_VERSION" "$LAMBDA_LAYER_1:$requested_layer1_version" "$LAMBDA_LAYER_2:$requested_layer2_version" --timeout "$LAMBDA_TIMEOUT" --memory-size "$LAMBDA_MEMORY_SIZE"
+  wait_lambda
+  aws lambda update-function-code --function-name "$LAMBDA_NAME" --zip-file "$lambda_archive" --publish
 fi
 aws lambda put-function-concurrency --function-name "$LAMBDA_NAME" --reserved-concurrent-executions "$LAMBDA_CONCURRENCY"
 echo "end script"
