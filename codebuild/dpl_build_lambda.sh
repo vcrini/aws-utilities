4710c2ba85efcf6362f713eef210bf70e5e08bd0
valerio <vcrini@gmail.com>
2023-10-19 16:10:13 +0200

    removing ipv6 parameter

--- codebuild/dpl_build_lambda.sh
+++ codebuild/dpl_build_lambda.sh
@@ -53,7 +53,7 @@ if [ "$CREATE_LAMBDA" = "true" ]; then
     aws lambda delete-event-source-mapping --uuid  "${delete_events[$i]}"
   done
 
-  aws lambda create-function --function-name "$LAMBDA_NAME" --zip-file "$lambda_archive" --handler "$LAMBDA_HANDLER" --runtime "$LAMBDA_RUNTIME" --role "$LAMBDA_ROLE" --layers "$LAMBDA_LAYER_0:$LAMBDA_LAYER_0_VERSION" "$LAMBDA_LAYER_1:$requested_layer1_version" "$LAMBDA_LAYER_2:$requested_layer2_version" --timeout "$LAMBDA_TIMEOUT" --memory-size "$LAMBDA_MEMORY_SIZE"  --vpc-config Ipv6AllowedForDualStack=false,SubnetIds="$LAMBDA_SUBNET",SecurityGroupIds="$LAMBDA_SECURITY_GROUP"
+  aws lambda create-function --function-name "$LAMBDA_NAME" --zip-file "$lambda_archive" --handler "$LAMBDA_HANDLER" --runtime "$LAMBDA_RUNTIME" --role "$LAMBDA_ROLE" --layers "$LAMBDA_LAYER_0:$LAMBDA_LAYER_0_VERSION" "$LAMBDA_LAYER_1:$requested_layer1_version" "$LAMBDA_LAYER_2:$requested_layer2_version" --timeout "$LAMBDA_TIMEOUT" --memory-size "$LAMBDA_MEMORY_SIZE"  --vpc-config SubnetIds="$LAMBDA_SUBNET",SecurityGroupIds="$LAMBDA_SECURITY_GROUP"
   aws lambda create-event-source-mapping --function-name "$LAMBDA_NAME" --event-source-arn "$QUEUE" --batch-size "$QUEUE_BATCH_SIZE" --maximum-batching-window-in-seconds "$QUEUE_BATCH_WINDOW" --scaling-config MaximumConcurrency="$QUEUE_MAXIMUM_CONCURRENCY"
   aws lambda create-event-source-mapping --function-name "$LAMBDA_NAME" --event-source-arn "$QUEUE2" --batch-size "$QUEUE2_BATCH_SIZE" --maximum-batching-window-in-seconds "$QUEUE2_BATCH_WINDOW" --scaling-config MaximumConcurrency="$QUEUE2_MAXIMUM_CONCURRENCY"
 else
